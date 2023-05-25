319a83f330a694f9e6e95b18da73ef148fec8497
ericwillyams <kw02191@gmail.com>
2023-05-25 19:37:45 -0400

    HBmenu

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello World!!!!")
+const previewMap = document.getElementById("previewMap")
+const locationInput=document.getElementById("locationInput")
+
+previewMap.addEventListener("click", (e) => {
+    console.log("PreviewMap!")
+})
+
+locationInput.addEventListener("input", (e) => {
+    console.log("Tell me Your Location!")
+})
+
