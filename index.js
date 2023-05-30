c164b24455af76684e0147a2562e5ff0138cef36
LaDonte LeBlanc <donte.11@hotmail.com>
2023-05-30 17:18:52 -0400

    testing of homepage with TA

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+function allowDrop(ev) {
+    ev.preventDefault();
+  }
+  
+  function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+  }
+  
+  function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+  }
+
+
+
 const previewMap = document.getElementById("previewMap")
 const locationInput=document.getElementById("locationInput")
 
