c66b1354c7df3afed203cd605ab202fa6f82b217
ericwillyams <kw02191@gmail.com>
2023-06-01 21:51:41 -0400

    map works

--- directions.js
+++ directions.js
@@ -1,17 +1,105 @@
-// To use directions in the Maps JavaScript API, create an object of type DirectionsService and call DirectionsService.route() to initiate a request to the Directions service, passing it a DirectionsRequest object literal containing the input terms and a callback method to execute upon receipt of the response.
+// let map;
 
-// The DirectionsRequest object literal contains the following fields:
-//     origin: LatLng | String | google.maps.Place,
-//     destination: LatLng | String | google.maps.Place,
-//     travelMode: TravelMode,
-//     transitOptions: TransitOptions,
-//     drivingOptions: DrivingOptions,
-//     unitSystem: UnitSystem,
-//     waypoints[]: DirectionsWaypoint,
-//     optimizeWaypoints: Boolean,
-//     provideRouteAlternatives: Boolean,
-//     avoidFerries: Boolean,
-//     avoidHighways: Boolean,
-//     avoidTolls: Boolean,
-//     region: String
-  
+// async function initMap() {
+//   const { Map } = await google.maps.importLibrary("maps");
+//   map = new Map(document.getElementById("map"), {
+//     center: { lat: -34.397, lng: 150.644 },
+//     zoom: 8,
+//   });
+// }
+
+// // initMap();
+
+// const DirectionsRequest = {
+//   origin: "Chicago, IL",
+//   destination: "Los Angeles, CA",
+//   travelMode: "DRIVING",
+//   //add others when functional
+// };
+
+// const directionsService = new google.map.DirectionsService();
+// directionsService.route();
+
+// directionsService.route(
+//     {
+//     origin: 'Chicago, IL',
+//     destination: 'Los Angeles, CA',
+//     travelMode: 'DRIVING',
+// });
+
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: { lat: 37.0902, lng: -95.7129 }, // Centered on the USA.
+  });
+  const directionsService = new google.maps.DirectionsService();
+  const directionsRenderer = new google.maps.DirectionsRenderer({
+    map: map,
+    panel: document.getElementById("panel"),
+  });
+
+  directionsRenderer.addListener("directions_changed", () => {
+    const directions = directionsRenderer.getDirections();
+
+    if (directions) {
+      computeTotalDistance(directions);
+    }
+  });
+
+  // const startFromInput = document.getElementById("startFromInput");
+  // const destinationInput = document.getElementById("destinationInput");
+
+  let startHere = document.querySelector("#startFromInput"); //Event Listener
+  let endHere = document.querySelector("#destinationInput"); //Event Listener
+  let startLoc = "";
+  let endLoc = "";
+
+  startHere.addEventListener("input", (e) => {
+    startLoc = startHere.value;
+    console.log(startLoc);
+  });
+
+  endHere.addEventListener("input", (e) => {
+    endLoc = endHere.value;
+    console.log(endLoc);
+  });
+
+  const previewMap = document.getElementById("previewMap");
+
+  previewMap.addEventListener("click", (e) => {
+    displayRoute(startLoc, endLoc, directionsService, directionsRenderer);
+  });
+}
+
+function displayRoute(origin, destination, service, display) {
+  service
+    .route({
+      origin: origin,
+      destination: destination,
+      travelMode: google.maps.TravelMode.DRIVING,
+    })
+    .then((result) => {
+      display.setDirections(result);
+    })
+    .catch((e) => {
+      // alert("Could not display directions due to: " + e);
+    });
+}
+
+function computeTotalDistance(result) {
+  let total = 0;
+  const myroute = result.routes[0];
+
+  if (!myroute) {
+    return;
+  }
+
+  for (let i = 0; i < myroute.legs.length; i++) {
+    total += myroute.legs[i].distance.value;
+  }
+
+  total = total / 1000;
+  document.getElementById("total").innerHTML = total + " km";
+}
+
+window.initMap = initMap;
--- index.js
+++ index.js
@@ -14,53 +14,53 @@ function allowDrop(ev) {
 
 
 
-const previewMap = document.getElementById("previewMap")
-const locationInput=document.getElementById("locationInput")
-const startFromInput = document.getElementById("startFromInput");
-const destinationInput = document.getElementById("destinationInput");
-const timeOfDay = document.getElementById("timeOfDay");
-const fastestSwitch = document.getElementById("fastestSwitch");
-const safestSwitch = document.getElementById("safestSwitch");
+// const previewMap = document.getElementById("previewMap")
+// const locationInput=document.getElementById("locationInput")
+// const startFromInput = document.getElementById("startFromInput");
+// const destinationInput = document.getElementById("destinationInput");
+// const timeOfDay = document.getElementById("timeOfDay");
+// const fastestSwitch = document.getElementById("fastestSwitch");
+// const safestSwitch = document.getElementById("safestSwitch");
 
 
 
 
-let map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 33.748, lng: 84.387 },
-    zoom: 8,
-  });
-}
+// let map;
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById("map"), {
+//     center: { lat: 33.748, lng: 84.387 },
+//     zoom: 8,
+//   });
+// }
 
 
 
-startFromInput.addEventListener("keypress", (e) => {
-  console.log("show your location") //here we will need to write a function that will bring in location data when a user begins entering input
-})
+// startFromInput.addEventListener("keypress", (e) => {
+//   console.log("show your location") //here we will need to write a function that will bring in location data when a user begins entering input
+// })
 
-locationInput.addEventListener("input", (e) => {
-    // console.log("Tell me Your!")
-})
-destinationInput.addEventListener("keypress", (e) => {
-  console.log("pull directions to map") //this event will pull direction info from API starting from the startFromInput event response
-})
+// locationInput.addEventListener("input", (e) => {
+//     // console.log("Tell me Your!")
+// })
+// destinationInput.addEventListener("keypress", (e) => {
+//   console.log("pull directions to map") //this event will pull direction info from API starting from the startFromInput event response
+// })
 
-timeOfDay.addEventListener("input", (e) => {
-  console.log("departure time, time until arrival, arrival time") //This event will determine if a user is departing at current time or a future time. This will pull data based off of input selected. 
-})
+// timeOfDay.addEventListener("input", (e) => {
+//   console.log("departure time, time until arrival, arrival time") //This event will determine if a user is departing at current time or a future time. This will pull data based off of input selected. 
+// })
 
-fastestSwitch.addEventListener("click", (e) => {  
-  console.log("Show Shortest Distance, Avoid Construction, Avoid Police, Take Highway")
-})//This event will filter data by shortest distance, construction, police presence, and highway routes
+// fastestSwitch.addEventListener("click", (e) => {  
+//   console.log("Show Shortest Distance, Avoid Construction, Avoid Police, Take Highway")
+// })//This event will filter data by shortest distance, construction, police presence, and highway routes
 
 
-safestSwitch.addEventListener("click", (e) => {
-  window.initMap = initMap;("Avoid Highways, Avoid Construction, Avoid heavy traffic, avoid accidents")
-})
+// safestSwitch.addEventListener("click", (e) => {
+//   window.initMap = initMap;("Avoid Highways, Avoid Construction, Avoid heavy traffic, avoid accidents")
+// })
 
-previewMap.addEventListener("click", (e) => {
-  console.log("click for page 3")// This event will take user to page 3 of app
-})
+// previewMap.addEventListener("click", (e) => {
+//   console.log("click for page 3")// This event will take user to page 3 of app
+// })
 
 
